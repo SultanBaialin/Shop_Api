021ed3bcd7963134257e68d71700f16b6230fad0
SultanBaialin <baialinovsultan@gmail.com>
2023-01-17 21:03:51 +0600

    16.01.2023

---
+++ account/send_mail.py
@@ -0,0 +1,13 @@
+from django.core.mail import send_mail
+
+
+def send_confirmation_email(user, code):
+    full_link = f'http://localhost:8000/api/v1/accounts/activate/{code}/'
+    send_mail(
+        'Здравствуйте активируйте ваш аккаунт!',
+        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link} ',
+        'baialinovsultan@gmail.com',
+        [user],
+        fail_silently= False
+    )
+
