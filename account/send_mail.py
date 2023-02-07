4adce3ac41f9d06f70b6476756614e016e49716d
SultanBaialin <baialinovsultan@gmail.com>
2023-02-07 18:54:06 +0600

    added link_server

--- account/send_mail.py
+++ account/send_mail.py
@@ -3,9 +3,10 @@ from django.core.mail import send_mail
 
 def send_confirmation_email(user, code):
     full_link = f'http://localhost:8000/api/v1/accounts/activate/{code}/'
+    full_link_server = f'http://34.16.136.138/api/v1/accounts/activate/{code}/'
     send_mail(
         'Здравствуйте активируйте ваш аккаунт!',
-        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link} ',
+        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link} \n{full_link_server} ',
         'baialinovsultan@gmail.com',
         [user],
         fail_silently=False
@@ -21,7 +22,7 @@ def send_confirmation_email(user, code):
 def send_notification(user_email, order_id, price):
     send_mail(
         'uvedomlenie o sozdanii zakaza!',
-        f"""vi sozdaly zakaz №{order_id}, \n polnaya stoimost' vashego zakaza: {price}. \nsposibo za to chto vibraly nas!""",
+        f"""vi sozdaly zakaz №{order_id}, \n polnaya stoimost' vashego zakaza: {price}. \nSpasibo za to chto vibraly nas!""",
         'from@example.com',
         [user_email],
         fail_silently=False
